506e78e18ed6eff9b910a2663050ad17dfe20794
Chenxia Liu <liuche@mozilla.com>
2011-11-07 12:46:10 -0500

    UI screens completed

---
+++ src/setup/main/AccountActivity.java
@@ -0,0 +1,13 @@
+package setup.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountActivity extends Activity {
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.account);
+  }
+  
+  
+}
---
+++ src/setup/main/SetupSyncActivity.java
@@ -0,0 +1,15 @@
+package setup.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupSyncActivity extends Activity {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.pair);
+    }
+
+
+}
